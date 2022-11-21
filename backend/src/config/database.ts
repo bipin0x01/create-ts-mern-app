8c4f26a65be425692b027619c921ee7b20c0efa0
Bipin Thapa <bt.kaji@gmail.com>
2022-11-22 00:14:49 +0545

    feat(db): config setup

---
+++ backend/src/config/database.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {});
+
+    console.log(`MongoDB Connected : ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+export default connectDB;
